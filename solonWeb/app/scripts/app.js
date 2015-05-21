0bd4020432e4530d9ddfc5aab552c2c9b5004850
Sonja <Sonja@Kristins-iMac.local>
2015-05-21 10:58:10 +0000

    SolonWeb files set up

---
+++ solonWeb/app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name solonWebApp
+ * @description
+ * # solonWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('solonWebApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ solonWeb/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name solonWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the solonWebApp
+ */
+angular.module('solonWebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
